eca3a0bd2817cd407177e079d2f878559057ec0a
AmoxV2 <60665323+AmoxV2@users.noreply.github.com>
2024-09-04 15:46:48 +0200

    Connecting To api to get Chart Data

--- trading_bot_turial.py
+++ trading_bot_turial.py
@@ -4,13 +4,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-client = Client(conf.api_key_biance,conf.secret_key_biance)
+client = Client(conf.Test_api_key_biance,conf.Test_secret_key_biance,testnet=True)
 
-data =client.get_account() #Sporawdzenie Konta
 
+data = client.get_account() #Sporawdzenie Konta
 #Przekształcanie danych z Api do DataFarme
-# balances_df=pd.DataFrame(data['balances'])
-# balances_df.to_csv('dane.csv',index=False)
+balances_df=pd.DataFrame(data['balances'])
+print(balances_df)
+#balances_df.to_csv('dane.csv',index=False)
 
 #dataStream przez websocekt ale na razie nie 
 
@@ -33,18 +34,21 @@ def getMinuteData(symbol,interval,lookback):
     return frame
 
 test = getMinuteData(symbol,interval,lookback)
-test.Open.plot()
+#test.Open.plot()
 
 
 #Strategy 
 #but if asset fell by more then 0.2% withnt the las 30 min 
 # sell if asset rises by more than 0.15% or falls furhter by 0.15 %
 
+
+
 def strategyTest(symbol,qty,entried=False): 
-    df = getMinuteData(symbol,'1m','30')
+    df = getMinuteData(symbol,'1m','15')
     cumulret = (df.Open.pct_change()+1).cumprod()-1
+    print("Cumulative Return:\n", cumulret)
     if not entried: 
-        if cumulret[-1] < -0.002:
+        if cumulret.iloc[-1] < -0.002:
             order = client.create_order(symbol=symbol,side='BUY',type='MARKET',quantity=qty)
             print(order)
             entried=True
@@ -52,11 +56,13 @@ def strategyTest(symbol,qty,entried=False):
             print("Nie było Trade")
     if entried:
         while True: 
-            df = getMinuteData(symbol,'1m','30')
+            df = getMinuteData(symbol,'1m','15')
             sinceBuy= df.loc[df.index > pd.to_datetime(order['transactTime'],unit='ms')]
             if len(sinceBuy) > 0:
                 sinceBuyRet = cumulret = (sinceBuy.Open.pct_change()+1).cumprod()-1
-                if sinceBuyRet[-1] > 0.0015 or sinceBuyRet[-1] < -0.0015:
+                if sinceBuyRet.iloc[-1] > 0.0015 or sinceBuyRet[-1] < -0.0015:
                     order = client.create_order(symbol=symbol,side='SELL',type='MARKET',quantity=qty)
                     print(order)
                     break
+                
+strategyTest('BTCUSDT',0.03)
